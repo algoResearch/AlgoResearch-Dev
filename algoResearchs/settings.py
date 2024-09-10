503fcaaec338ff90ee38e3e82a68148a4e26e8ed
rc10283 <99059695+rc10283@users.noreply.github.com>
2024-09-10 11:40:38 -0400

    Update dependencies

--- algoResearchs/settings.py
+++ algoResearchs/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
+DATABASES = {
+    'default': dj_database_url.config(default='postgres://USER:PASSWORD@HOST:PORT/DBNAME')
+}
+
 
 
 
@@ -33,7 +37,7 @@ DEBUG = False
 
 
 # Application definition
-ALLOWED_HOSTS = ['algoResearchs.herokuapp.com', 'localhost', '127.0.0.1', 'ryanccarmody.com']
+ALLOWED_HOSTS = ['algoResearchs.herokuapp.com', 'localhost', '127.0.0.1', 'www.ryanccarmody.com']
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
@@ -111,10 +115,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Directory for `collectstatic` in production
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 
 MEDIA_URL = '/media/'
