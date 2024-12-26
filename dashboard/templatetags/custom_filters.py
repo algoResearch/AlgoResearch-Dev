59d303a730075f3015478f7409f9ae7d65effc83
rc10283 <99059695+rc10283@users.noreply.github.com>
2024-12-26 12:54:32 -0500

    Mime Type Again

--- dashboard/templatetags/custom_filters.py
+++ dashboard/templatetags/custom_filters.py
@@ -1,6 +1,16 @@
 from django import template
-
+import mimetypes
+from django import template
 register = template.Library()
+
+@register.filter
+def file_mimetype(file_name):
+    """
+    Returns the MIME type of a file based on its name.
+    """
+    mime_type, _ = mimetypes.guess_type(file_name)
+    return mime_type or "unknown"
+
 
 @register.filter
 def file_extension(filename, extensions):
